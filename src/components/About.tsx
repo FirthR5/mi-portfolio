8f06af8df0467f4d551bd1f145fc34beed8a06b5
FirthR5 <47197502+FirthR5@users.noreply.github.com>
2026-09-10 21:24:27 -0500

    fix: disable noUnusedLocals in tsconfig

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,8 @@
 import { useLang } from '../context/LanguageContext'
 import { education, summary } from '../data/profile'
 
+const summaryParagraph = summary.paragraph
+
 export default function About() {
   const { t } = useLang()
 
